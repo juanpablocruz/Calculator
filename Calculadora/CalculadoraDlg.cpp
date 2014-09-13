a4f5cbddf1213a9353e428c564ffc72e5f62898d
Juan Pablo <juanpablocruzmaseda@gmail.com>
2014-09-13 17:54:06 +0200

    Cientifica añadida

--- Calculadora/CalculadoraDlg.cpp
+++ Calculadora/CalculadoraDlg.cpp
@@ -1,4 +1,4 @@
-
+﻿
 // CalculadoraDlg.cpp : implementation file
 //
 
@@ -8,11 +8,13 @@
 #include "afxdialogex.h"
 #include <math.h>
 #include <vector>
+#include <iostream>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
+#define PI 3.14159265359
 
 // CAboutDlg dialog used for App About
 
@@ -58,6 +60,22 @@ BEGIN_DHTML_EVENT_MAP(CCalculadoraDlg)
 	DHTML_EVENT_ONCLICK(_T("porcen"), porcen)
 	DHTML_EVENT_ONCLICK(_T("erase"), erase)
 	DHTML_EVENT_ONCLICK(_T("mod"), mod)
+	DHTML_EVENT_ONCLICK(_T("standard"), standardMode)
+	DHTML_EVENT_ONCLICK(_T("cient"), cientMode)
+	DHTML_EVENT_ONCLICK(_T("powe"), powe_func)
+	DHTML_EVENT_ONCLICK(_T("pow"), pow_func)
+	DHTML_EVENT_ONCLICK(_T("pi"), show_pi)
+	DHTML_EVENT_ONCLICK(_T("square"), square)
+	DHTML_EVENT_ONCLICK(_T("sin"), sin_func)
+	DHTML_EVENT_ONCLICK(_T("cos"), cos_func)
+	DHTML_EVENT_ONCLICK(_T("arctan"), arctan_func)
+	DHTML_EVENT_ONCLICK(_T("arcsin"), arcsin_func)
+	DHTML_EVENT_ONCLICK(_T("arccos"), arccos_func)
+	DHTML_EVENT_ONCLICK(_T("tan"), tan_func)
+	DHTML_EVENT_ONCLICK(_T("deg"), changeAngle)
+	DHTML_EVENT_ONCLICK(_T("neper"), neper)
+	DHTML_EVENT_ONCLICK(_T("log"), log_func)
+	DHTML_EVENT_ONCLICK(_T("factorial"), fact_func)
 	DHTML_EVENT_ONCLICK(_T("ButtonOK"), OnButtonOK)
 	DHTML_EVENT_ONCLICK(_T("ButtonCancel"), OnButtonCancel)
 END_DHTML_EVENT_MAP()
@@ -183,6 +201,10 @@ std::string& BstrToStdString(const BSTR bstr, std::string& dst, int cp = CP_UTF8
 	return dst;
 }
 
+float round(float f, float pres)
+{
+	return (float)round(f*pow(10, pres+1)) / pow(10, pres+1);
+}
 
 BSTR Convert(const std::string& s) { return CComBSTR(s.c_str()).Detach(); }
 
@@ -355,8 +377,7 @@ HRESULT CCalculadoraDlg::erase(IHTMLElement* pElement)
 	{
 		_variant_t val;
 		textArea->getAttribute(_bstr_t("value"), 0, &val);
-		SAFEARRAY* vect;
-		
+
 		std::string str,st;
 
 		// convert directly into str-allocated buffer.
@@ -393,7 +414,10 @@ HRESULT CCalculadoraDlg::igual(IHTMLElement* pElement)
 			textArea->put_innerText(_bstr_t(this->leftValue * this->rightValue));
 			break;
 		case OPERADOR_DIV:
+			if (this->rightValue != 0)
 			textArea->put_innerText(_bstr_t(this->leftValue / this->rightValue));
+			else
+				textArea->put_innerText(_bstr_t("Inf"));
 			break;
 		case OPERADOR_PORCEN:
 			textArea->put_innerText(_bstr_t((this->leftValue *100 )/ this->rightValue));
@@ -401,9 +425,398 @@ HRESULT CCalculadoraDlg::igual(IHTMLElement* pElement)
 		case OPERADOR_MOD:
 			textArea->put_innerText(_bstr_t(((INT)this->leftValue) % (INT)this->rightValue));
 			break;
+		case OPERADOR_POW:
+			textArea->put_innerText(_bstr_t(pow(this->leftValue, this->rightValue)));
+			break;
+		case OPERADOR_2EXP:
+			textArea->put_innerText(_bstr_t((this->rightValue)*(this->rightValue)));
+			break;
 		}
 	}
 	this->res = OPERADOR_IGUAL;
 	
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::cientMode(IHTMLElement* pElement)
+{
+	pElement->put_className(_bstr_t("active"));
+	IHTMLElement* active = NULL;
+	if (GetElement(_T("standard"), &active) == S_OK && active != NULL){
+		active->removeAttribute(_bstr_t("className"), 1, 0);
+	}
+	IHTMLElement* cien = NULL;
+	if (GetElement(_T("cientific"), &cien) == S_OK && cien != NULL){
+		cien->put_className(_bstr_t("active"));
+	}
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::standardMode(IHTMLElement* pElement)
+{
+
+	pElement->put_className(_bstr_t("active"));
+	IHTMLElement* active = NULL;
+	if (GetElement(_T("cient"), &active) == S_OK && active != NULL){
+		active->removeAttribute(_bstr_t("className"), 1,0);
+	}
+	IHTMLElement* cien = NULL;
+	if (GetElement(_T("cientific"), &cien) == S_OK && cien != NULL){
+		cien->removeAttribute(_bstr_t("className"), 1, 0);
+	}
+	return S_OK;
+}
+
+// CIENTIFIC FUNCTIONS
+
+HRESULT CCalculadoraDlg::powe_func(IHTMLElement* pElement)
+{
+	this->operador = OPERADOR_POWE;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		textArea->put_innerText(_bstr_t(exp(this->leftValue)));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::pow_func(IHTMLElement* pElement)
+{
+	this->operador = OPERADOR_POW;
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::show_pi(IHTMLElement* pElement)
+{
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		textArea->put_innerText(_bstr_t(PI));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::square(IHTMLElement *pElement) {
+	this->operador = OPERADOR_2EXP;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		textArea->put_innerText(_bstr_t((this->leftValue)*(this->leftValue)));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::sin_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_SIN;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		double angulo;
+		switch (system_grad){
+
+		case RAD:
+			angulo = this->leftValue;
+			break;
+		case DEG:
+			angulo = this->leftValue * PI / 180;
+			break;
+		case GRAD:
+			angulo = this->leftValue * PI / 200;
+			break;
+		}
+
+		textArea->put_innerText(_bstr_t(round(sin(angulo),2)));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::cos_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_COS;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		double angulo;
+		switch (system_grad){
+
+		case RAD:
+			angulo = this->leftValue;
+			break;
+		case DEG:
+			angulo = this->leftValue * PI / 180;
+			break;
+		case GRAD:
+			angulo = this->leftValue * PI / 200;
+			break;
+		}
+
+		textArea->put_innerText(_bstr_t(round(cos(angulo), 2)));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::tan_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_TAN;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		double angulo;
+		switch (system_grad){
+
+		case RAD:
+			angulo = this->leftValue;
+			break;
+		case DEG:
+			angulo = this->leftValue * PI / 180;
+			break;
+		case GRAD:
+			angulo = this->leftValue * PI / 200;
+			break;
+		}
+
+		textArea->put_innerText(_bstr_t(round(tan(angulo), 2)));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+HRESULT CCalculadoraDlg::arcsin_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_ASIN;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		double angulo;
+		switch (system_grad){
+
+		case RAD:
+			angulo = asin(this->leftValue);
+			break;
+		case DEG:
+			angulo = asin(this->leftValue) * 180 / PI;
+			break;
+		case GRAD:
+			angulo = asin(this->leftValue) * 200 / PI;
+			break;
+		}
+		textArea->put_innerText(_bstr_t(angulo));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::arccos_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_ACOS;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		double angulo;
+		switch (system_grad){
+
+		case RAD:
+			angulo = acos(this->leftValue);
+			break;
+		case DEG:
+			angulo = acos(this->leftValue) * 180 / PI;
+			break;
+		case GRAD:
+			angulo = acos(this->leftValue) * 200 / PI;
+			break;
+		}
+		textArea->put_innerText(_bstr_t(angulo));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::arctan_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_ATAN;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		double angulo;
+		switch (system_grad){
+
+		case RAD:
+			angulo = atan(this->leftValue);
+			break;
+		case DEG:
+			angulo = atan(this->leftValue) * 180 / PI;
+			break;
+		case GRAD:
+			angulo = atan(this->leftValue) * 200 / PI;
+			break;
+		}
+		textArea->put_innerText(_bstr_t(angulo));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::changeAngle(IHTMLElement* pElement)
+{
+	IHTMLElement* next = NULL;
+	IHTMLElement* prev = NULL;
+	LPCTSTR type = NULL;
+	LPCTSTR type2 = NULL;
+	switch (system_grad){
+	case RAD:
+		type = _T("grad_angle");
+		type2 = _T("rad_angle");
+		system_grad = GRAD;
+		break;
+	case DEG:
+		type = _T("rad_angle");
+		type2 = _T("deg_angle");
+		system_grad = RAD;
+		break;
+	case GRAD:
+		type = _T("deg_angle");
+		type2 = _T("grad_angle");
+		system_grad = DEG;
+		break;
+	}
+	if (GetElement(type, &next) == S_OK &&
+		next != NULL)
+	{
+		next->put_className(_bstr_t("active_angle"));
+	}
+	if (GetElement(type2, &prev) == S_OK &&
+		prev != NULL)
+	{
+		prev->removeAttribute(_bstr_t("className"), 1, 0);
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::neper(IHTMLElement *pElement) {
+	this->operador = OPERADOR_NEPER;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		
+		textArea->put_innerText(_bstr_t(log(this->leftValue)));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::log_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_LOG;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+
+		textArea->put_innerText(_bstr_t(log10(this->leftValue)));
+		this->res = OPERADOR_IGUAL;
+	}
+
+	return S_OK;
+}
+
+HRESULT CCalculadoraDlg::fact_func(IHTMLElement *pElement) {
+	this->operador = OPERADOR_FACT;
+
+	IHTMLElement* textArea = NULL;
+	if (GetElement(_T("textArea"), &textArea) == S_OK &&
+		textArea != NULL)
+	{
+		_variant_t val;
+		textArea->getAttribute(_bstr_t("value"), 0, &val);
+		VarR4FromStr(val.bstrVal, 0, 0, &this->leftValue);
+
+		long long factorial = 1;
+		for (int i = 1; i <= (int)round(this->leftValue); i++){
+			factorial *= i;
+		}
+
+		textArea->put_innerText(_bstr_t(factorial));
+		this->res = OPERADOR_IGUAL;
+	}
+
 	return S_OK;
 }
--- Calculadora/CalculadoraDlg.h
+++ Calculadora/CalculadoraDlg.h
@@ -16,7 +16,13 @@ public:
 	enum { IDD = IDD_CALCULADORA_DIALOG, IDH = IDR_HTML_CALCULADORA_DIALOG };
 	FLOAT leftValue, rightValue;
 	int operador,res=0;
-	enum { OPERADOR_SUMA, OPERADOR_RESTA, OPERADOR_MULTI, OPERADOR_DIV, OPERADOR_IGUAL, OPERADOR_RAIZ, OPERADOR_PORCEN, OPERADOR_MOD };
+	enum {DEG,RAD,GRAD};
+	int system_grad = DEG;
+	enum { OPERADOR_SUMA, OPERADOR_RESTA, OPERADOR_MULTI, OPERADOR_DIV, OPERADOR_IGUAL, 
+		OPERADOR_RAIZ, OPERADOR_PORCEN, OPERADOR_MOD, OPERADOR_POWE, OPERADOR_POW, OPERADOR_2EXP,
+		OPERADOR_SIN, OPERADOR_COS, OPERADOR_TAN, OPERADOR_ASIN, OPERADOR_ACOS, OPERADOR_ATAN,
+		OPERADOR_DEC,OPERADOR_NEPER, OPERADOR_LOG, OPERADOR_FACT
+	};
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
 
@@ -32,6 +38,24 @@ public:
 	HRESULT porcen(IHTMLElement *pElement);
 	HRESULT erase(IHTMLElement *pElement);
 	HRESULT mod(IHTMLElement *pElement);
+
+	HRESULT cientMode(IHTMLElement *pElement);
+	HRESULT standardMode(IHTMLElement *pElement);
+
+	HRESULT powe_func(IHTMLElement *pElement);
+	HRESULT pow_func(IHTMLElement *pElement);
+	HRESULT show_pi(IHTMLElement *pElement);
+	HRESULT square(IHTMLElement *pElement);
+	HRESULT sin_func(IHTMLElement *pElement);
+	HRESULT cos_func(IHTMLElement *pElement);
+	HRESULT tan_func(IHTMLElement *pElement);
+	HRESULT arcsin_func(IHTMLElement *pElement);
+	HRESULT arccos_func(IHTMLElement *pElement);
+	HRESULT arctan_func(IHTMLElement *pElement);
+	HRESULT changeAngle(IHTMLElement *pElement);
+	HRESULT neper(IHTMLElement *pElement);
+	HRESULT log_func(IHTMLElement *pElement);
+	HRESULT fact_func(IHTMLElement *pElement);
 
 // Implementation
 protected:
